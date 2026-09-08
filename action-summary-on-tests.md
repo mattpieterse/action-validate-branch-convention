b094a18a5ccfa80dd1eedcd80c93a5d0bc28359e
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2026-09-08 21:31:00 +0200

    fix(workflows/test): fix tag detection handling and improve test-environment experience

---
+++ action-summary-on-tests.md
@@ -0,0 +1,2 @@
+> [!CAUTION]
+> This action was run using input variables that override real-world GitHub References and thus the output may not accurately validate the genuine environment context. **For production validation, remove the `branch` input and rely on the actual workflow context.**
--- action.js
+++ action.js
@@ -24,7 +24,7 @@ try {
 
     const githubRef = process.env.GITHUB_REF;
 
-    if ((githubRef || branch).startsWith('refs/tags/')) {
+    if ((branch || githubRef).startsWith('refs/tags/')) {
         console.log(`ℹ️ Skipping branch validation for tag refs: ${githubRef || branch}`);
         process.exit(0);
     }
@@ -35,13 +35,28 @@ try {
 
     if (regex.test(branch)) {
         console.log('✅ Follows accepted conventions.');
+        
+        if (inputBranch) {
+            console.log('⚠️ Testing input-variables were used and override the real-world values.');
+            const summary = readFileSync(new URL('./action-summary-on-tests.md', import.meta.url), 'utf8');
+            appendFileSync(process.env.GITHUB_STEP_SUMMARY, summary);
+        }
+
         process.exit(0);
     } else {
         console.log(`❌ Invalid branch name.`);
         console.log('See summary for details.');
 
-        let summary = readFileSync(new URL('./action.md', import.meta.url), 'utf8')
+        let summary = readFileSync(new URL('./action-summary-on-error.md', import.meta.url), 'utf8')
             .replace('{{BRANCH_NAME}}', branch);
+
+        if (inputBranch) {
+            console.log('⚠️ Testing input-variables were used and override the real-world values.');
+            const testWarning = readFileSync(new URL('./action-summary-on-tests.md', import.meta.url), 'utf8');
+            summary = summary.replace('{{TEST_WARNING}}', testWarning);
+        } else {
+            summary = summary.replace('{{TEST_WARNING}}\n\n', '');
+        }
 
         appendFileSync(process.env.GITHUB_STEP_SUMMARY, summary);
         process.exit(1);
